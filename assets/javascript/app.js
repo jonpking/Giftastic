357bb03951005198ef8d25a5ab49b307a6e385ba
Enigma <jonpking@gmail.com>
2019-06-24 21:40:04 -0700

    added comments to clarify some tasks in code and to outline others that still need done, placed for loop into function and added code to prevent button duplication, updated id selection in for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,22 @@
 
+// array of search strings
 const topics = ["batman", "spider-man", "x-men"];
 
-for (let i = 0; i < topics.length; i++) {
-    const a = $("<button>");
-    a.text(topics[i]);
-    $("#gifArea").append(a);
-}
+// creates a button on the page for each item in the topics array
+function createButtons() {
+
+    $("buttonArea").empty();
+    for (let i = 0; i < topics.length; i++) {
+        const a = $("<button>");
+        a.text(topics[i]);
+        $("#buttonArea").append(a);
+    }
+};
+
+// click button to grab 10 static, non-animated gifs from api and place them on page
+
+// click a gif and it animates, click again and it stops
+
+// under each gif, display rating
+
+// input box to add new buttons to topics array and remakes the buttons on the page

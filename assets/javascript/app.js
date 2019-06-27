2ad5be415b956d33e631a60137e827d3c5b05049
Enigma <jonpking@gmail.com>
2019-06-26 20:24:19 -0700

    added code to start and stop gifs playing when clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,7 @@ function createButtons() {
     }
 };
 
-// ajax call
+// uses ajax call to pull gifs from giphy api and displays them, along with their ratings, to the page
 function getApiInfo() {
     const comic = $(this).attr("data-name");
     const queryURL = "http://api.giphy.com/v1/gifs/search?api_key=W5rOVNC5OWIuMkDJ3o5vDoDBxFuqcCv7&limit=10&q=" + comic;
@@ -24,27 +24,34 @@ function getApiInfo() {
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        for (let i = 0; i < response.data.length; i++){
-        const comicDiv = $("<div class='comic'>");
-        const gifImage = response.data[i].images.fixed_height_still.url;
-        const itemOne = $("<img>").attr("src", gifImage);
-        comicDiv.append(itemOne);
-        const rating = response.data[i].images.rating;
-        const itemTwo = $("<p>").text("Rating: " + rating);
-        comicDiv.append(itemTwo);
-
-        $("#gifArea").prepend(comicDiv);
+        for (let i = 0; i < response.data.length; i++) {
+            const comicDiv = $("<div class='comic'>");
+            const gifStill = response.data[i].images.fixed_height_still.url;
+            const gifAnimate = response.data[i].images.fixed_height.url;
+            const gifImage = $("<img>").attr("src", gifStill).attr("class", "gif").attr("data-state", "still").attr("data-still", gifStill).attr("data-animate", gifAnimate);
+            comicDiv.append(gifImage);
+            const rating = response.data[i].images.rating;
+            const ratingText = $("<p>").text("Rating: " + rating);
+            comicDiv.append(ratingText);
+            $("#gifArea").prepend(comicDiv);
         }
     });
 }
 
-// click button to grab 10 static, non-animated gifs from api and place them on page
-
 // click a gif and it animates, click again and it stops
-
-// under each gif, display rating
+function animateGif() {
+    const state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+};
 
 // input box to add new buttons to topics array and remakes the buttons on the page
 
-$(document).on("click", ".topicButton", getApiInfo); 
+$(document).on("click", ".gif", animateGif);
+$(document).on("click", ".topicButton", getApiInfo);
 createButtons();
